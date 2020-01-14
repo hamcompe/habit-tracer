84acdb7aa088e3cdc76e86583303c42e7531bbbb
Pallop Chaoputhipuchong <pallop.chao@gmail.com>
2020-01-14 17:28:05 +0700

    Add new calendar

---
+++ src/components/calendar.js
@@ -0,0 +1,106 @@
+import React from "react"
+import styled from "@emotion/styled"
+import { css } from "@emotion/core"
+import tw from "tailwind.macro"
+
+const CalendarGrid = styled.section`
+  font-variant-numeric: tabular-nums;
+  font-feature-settings: tnum;
+  text-align: right;
+`
+const DayTitleSegment = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  ${tw`mb-1 text-gray-700 font-semibold`};
+`
+const DayNumberSegment = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+`
+const DayList = styled.li`
+  ${tw`text-gray-900 hover:bg-gray-200 border-r border-b p-2`};
+  &:nth-of-type(-n + 7) {
+    ${tw`border-t`};
+  }
+  &:nth-of-type(7n + 1) {
+    ${tw`border-l`};
+  }
+  ${props => props.unFocus && tw`text-gray-500`};
+  ${props => props.checked && tw`bg-gray-400 hover:bg-gray-500`};
+`
+
+const getMonthYear = date =>
+  date.toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "long",
+  })
+
+const getStartDay = date =>
+  new Date(date.getFullYear(), date.getMonth()).getDay()
+const getBeginOfMonth = date => new Date(date.getFullYear(), date.getMonth())
+const addDay = (date, incrementalDay) => {
+  const newDate = new Date(date)
+  newDate.setDate(date.getDate() + incrementalDay)
+  return newDate
+}
+function range({ start, end, num }) {
+  const length = num || end - start + 1
+  return Array.from({ length }).map((_, idx) => start + idx)
+}
+
+const TOTAL_DAY_CALENDAR = 42
+
+const Calendar = ({
+  focusDate = new Date(),
+  didAtList = [new Date("2020-01-07"), new Date("2020-01-14")],
+  onClick = date => {
+    alert(`Clicked :${date.date.toDateString()}, is checked: ${date.isDid}`)
+  },
+}) => {
+  const lookBackNum = getStartDay(focusDate)
+  const beginOfFocusDate = getBeginOfMonth(focusDate)
+  const incrementalDayList = range({
+    start: -lookBackNum,
+    num: TOTAL_DAY_CALENDAR,
+  })
+  const calendarDayList = incrementalDayList.map(incDay =>
+    addDay(beginOfFocusDate, incDay)
+  )
+  const thisMonth = focusDate.getMonth()
+
+  const isDid = date =>
+    !!didAtList.find(
+      didAt => didAt.toLocaleDateString() === date.toLocaleDateString()
+    )
+
+  return (
+    <CalendarGrid>
+      <h2 css={tw`text-l font-semibold text-center mb-4 mt-6`}>
+        {getMonthYear(focusDate)}
+      </h2>
+      <DayTitleSegment>
+        <li>Sun</li>
+        <li>Mon</li>
+        <li>Tue</li>
+        <li>Wed</li>
+        <li>Thu</li>
+        <li>Fri</li>
+        <li>Sat</li>
+      </DayTitleSegment>
+      <DayNumberSegment>
+        {calendarDayList.map(date => (
+          <DayList
+            key={date.getTime()}
+            checked={isDid(date)}
+            unFocus={thisMonth !== date.getMonth()}
+            onClick={() => onClick({ isDid: isDid(date), date: date })}
+          >
+            {date.getDate()}
+          </DayList>
+        ))}
+      </DayNumberSegment>
+    </CalendarGrid>
+  )
+}
+
+export default Calendar
--- src/pages/tasks.js
+++ src/pages/tasks.js
@@ -4,29 +4,10 @@ import queryString from "query-string"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import styled from "@emotion/styled"
-import { css } from "@emotion/core"
 import tw from "tailwind.macro"
 import { FirebaseContext } from "gatsby-plugin-firebase"
 import LoadingSpinner from "../components/loading-spinner"
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-`
-const Column = styled.div`
-  ${tw`bg-gray-400 h-12`};
-  ${props => props.marked && tw`bg-gray-600`};
-`
-const HeaderColumn = styled.div`
-  ${tw`bg-gray-500 h-12`};
-`
-
-const getNumListByLength = length =>
-  Array.from({ length }).map((_, idx) => idx + 1)
-const getDaysInMonth = (year, month) => new Date(year, month, 0).getDate()
-const getDateFromDay = ({ year = 2020, month = 0, day }) =>
-  new Date(year, month, day)
+import Calendar from "../components/calendar"
 
 const IndexPage = ({ location }) => {
   const { id } = queryString.parse(location.search) || {}
@@ -71,9 +52,6 @@ const IndexPage = ({ location }) => {
       })
   }
 
-  const numDays = getDaysInMonth(2020, 1)
-  const days = getNumListByLength(numDays)
-
   return (
     <Layout>
       <SEO title="Tasks" />
@@ -82,40 +60,23 @@ const IndexPage = ({ location }) => {
       {loading ? (
         <LoadingSpinner />
       ) : (
-        <Grid>
-          <HeaderColumn>Sun</HeaderColumn>
-          <HeaderColumn>Mon</HeaderColumn>
-          <HeaderColumn>Tue</HeaderColumn>
-          <HeaderColumn>Wed</HeaderColumn>
-          <HeaderColumn>Thu</HeaderColumn>
-          <HeaderColumn>Fri</HeaderColumn>
-          <HeaderColumn>Sat</HeaderColumn>
-          {days.map(day => {
-            const hasDone = didAtList.find(didAt => didAt.getDate() === day)
-            return (
-              <Column
-                key={day}
-                onClick={() => {
-                  if (hasDone) {
-                    const removedDayList = didAtList.filter(
-                      didAt => didAt.getDate() !== day
-                    )
-                    setDidAtList(removedDayList)
-                    updateList(removedDayList)
-                  } else {
-                    const newDate = getDateFromDay({ day })
-                    const newDidAtList = didAtList.concat(newDate)
-                    setDidAtList(newDidAtList)
-                    updateList(newDidAtList)
-                  }
-                }}
-                marked={hasDone}
-              >
-                {day}
-              </Column>
-            )
-          })}
-        </Grid>
+        <Calendar
+          focusDate={new Date()}
+          didAtList={didAtList}
+          onClick={({ isDid, date }) => {
+            if (isDid) {
+              const removedDayList = didAtList.filter(
+                didAt => didAt.toJSON() !== date.toJSON()
+              )
+              setDidAtList(removedDayList)
+              updateList(removedDayList)
+            } else {
+              const newDidAtList = didAtList.concat(date)
+              setDidAtList(newDidAtList)
+              updateList(newDidAtList)
+            }
+          }}
+        />
       )}
     </Layout>
   )
